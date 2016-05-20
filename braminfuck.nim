808b4cf3312cc6f0d6636c212a72034f64c32d65
k0pernicus <antonin.carette@gmail.com>
2016-05-19 21:02:24 -0400

    Debug

--- braminfuck.nim
+++ braminfuck.nim
@@ -17,18 +17,18 @@ import os
 
 let program_arguments = docopt(doc, version="0.1.0")
 
-proc `$`(value: int8): char =
+proc print_char(value: uint8): char =
   chr(value)
 
-proc put_char(): int8 =
-  ord(stdin.readChar).int8
+proc put_char(): uint8 =
+  ord(stdin.readChar).uint8
 
 # Initialize cells, data_pointer
 # The cells field is a field to store program instructions
 # The cells_pointer is a field to remember our position in the cells field
 # The data_pointer is just a pointer to the current instruction
 var
-  cells : seq[int8]
+  cells : seq[uint8]
 
 proc run(code: string) =
 
@@ -38,29 +38,31 @@ proc run(code: string) =
     # for '[' and ']' cases
     casual_statement: seq[int32] = newSeq[int32]()
 
-  cells = newSeq[int8]()
+  cells = newSeq[uint8]()
 
   while data_pointer <= code.len:
 
     if cells_pointer == cells.len:
       cells.add(0)
 
+    #echo "=> ", code[data_pointer]
+
     case code[data_pointer]:
       of '>': inc(cells_pointer)
       of '<': dec(cells_pointer)
       of '+': inc(cells[cells_pointer])
       of '-': dec(cells[cells_pointer])
-      of '.': stdout.write($cells[cells_pointer])
+      of '.': stdout.write(print_char(cells[cells_pointer]))
       of ',': cells[cells_pointer] = put_char()
       of '[':
         if cells[cells_pointer] == 0:
           var open_brackets = 0
-          while code[data_pointer] != ']' or open_brackets != 0:
-            inc(data_pointer)
+          inc(data_pointer)
+          while code[data_pointer] == ']' or open_brackets > 0:
             if code[data_pointer] == '[': inc(open_brackets)
-            if code[data_pointer] == ']' and open_brackets != 0:
+            if code[data_pointer] == ']' and open_brackets > 0:
               dec(open_brackets)
-              inc(data_pointer)
+            inc(data_pointer)
         else:
           casual_statement.add(data_pointer)
       of ']':
